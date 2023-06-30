cd8b52968d7b7d47d3f5ffcd109ad0231ea4fd6b
Archy <antoine.tassez@gmail.com>
2023-06-30 12:58:57 +0200

    Moved models from "main/models" to "feed/models.py"

--- lit_review/feed/models.py
+++ lit_review/feed/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.conf import settings
 
-# Create your models here.
+
+# LITReview main Models.
+class Ticket(models.Model):
+    # Your Ticket model definition goes here
+    title = models.CharField(max_length=128)
+    description = models.CharField(max_length=2048, blank=True)
+    user = models.ForeignKey(to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    image = models.ImageField(null=True, blank=True)
+    time_created = models.DateTimeField(auto_now_add=True)
+
+class Review(models.Model):
+    ticket = models.ForeignKey(to=Ticket, on_delete=models.CASCADE)
+    rating = models.PositiveSmallIntegerField(
+        # validates that rating must be between 0 and 5
+        validators=[MinValueValidator(0), MaxValueValidator(5)])
+    user = models.ForeignKey(
+        to=settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    headline = models.CharField(max_length=128)
+    body = models.CharField(max_length=8192, blank=True)
+    time_created = models.DateTimeField(auto_now_add=True)
