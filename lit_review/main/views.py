4409edbdf8ec9add0f41d328bdd85c8e878359b9
Archy <antoine.tassez@gmail.com>
2023-06-30 12:57:35 +0200

    Moved models from "main/models" to "feed/models.py"

--- lit_review/main/views.py
+++ lit_review/main/views.py
@@ -1,11 +1,7 @@
-from itertools import chain
+from django.shortcuts import redirect
 
-from django.db.models import CharField, Value
-from django.shortcuts import render, redirect
-
-from .models import Review, Ticket
 
 def homepage(request):
     if request.user.is_authenticated:
-        return redirect("users:registered_home")
+        return redirect("feed:home")
     return redirect("users:authentication_page")
