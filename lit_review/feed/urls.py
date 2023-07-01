0f55d97739049b7d9f0b41180ab67225e77101b5
Archy <antoine.tassez@gmail.com>
2023-07-01 16:47:44 +0200

    Added url redirection in feed

--- lit_review/feed/urls.py
+++ lit_review/feed/urls.py
@@ -6,6 +6,8 @@ from . import views
 app_name = "feed"
 
 urlpatterns = [
-    path("home/", views.user_feed, name="home"),
-    path("new_ticket/", views.ticket_creation, name="ticket_creation"),
+    path("home/", views.render_user_feed, name="home"),
+    path("new_ticket/", views.create_new_ticket_request, name="ticket_creation"),
+    path("new_review/", views.create_new_review_request, name="review_creation"),
+    path("new_review/<int:ticket_id>", views.respond_to_ticket_request, name="review_creation"),
 ]
