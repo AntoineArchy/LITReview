5936c54af72beece06aafa877f7cf49330326bee
Archy <antoine.tassez@gmail.com>
2023-07-01 16:45:34 +0200

    Functions refactoring and name clarification

--- lit_review/users/urls.py
+++ lit_review/users/urls.py
@@ -5,9 +5,8 @@ from . import views
 app_name = "users"
 
 urlpatterns = [
-    path("registration/", views.register_user, name="register_user"),
+    path("registration/", views.user_registration_request, name="register_user"),
     path("authentication_page/", views.authentication_request, name="authentication_page"),
-    path("login/", views.login_user, name="login"),
     path("logout/", views.logout_user, name="logout"),
 ]
 
--- lit_review/users/views.py
+++ lit_review/users/views.py
@@ -1,32 +1,10 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth import login, logout, authenticate
+from django.contrib import messages
 
 
-def authentication_request(request):
-    if request.method == 'POST':
-        return login_user(request)
-    form = AuthenticationForm()
-    return render(request,
-                  "users/authentication_page.html",
-                  context={"form": form})
-
-
-def register_user(request):
-    form = UserCreationForm(request.POST)
-    if form.is_valid():
-        user = form.save()
-        login(request, user)
-        return redirect("main:homepage")
-    else:
-        for msg in form.error_messages:
-            print(form.error_messages[msg])
-    return render(request,
-                  'forms/registration_form.html',
-                  context={"form":form})
-
-
-def login_user(request):
+def post_authentication_request(request):
     if request.method == 'POST':
         form = AuthenticationForm(request, data=request.POST)
         if form.is_valid():
@@ -36,9 +14,41 @@ def login_user(request):
             if user is not None:
                 login(request, user)
         else:
-            for msg in form.error_messages:
-                print(form.error_messages[msg])
+            for err_key, msg in form.errors.items():
+                messages.error(request, msg)
     return redirect("main:homepage")
+
+
+def authentication_request(request):
+    if request.method == 'POST':
+        return post_authentication_request(request)
+    form = AuthenticationForm()
+    return render(request,
+                  "users/authentication_page.html",
+                  context={"form": form})
+
+
+def post_user_registration_request(request):
+    form = UserCreationForm(request.POST)
+    if form.is_valid():
+        user = form.save()
+        login(request, user)
+        return redirect("main:homepage")
+    else:
+        for err_key, msg in form.errors.items():
+            messages.error(request, msg)
+    return render(request,
+                  'forms/users/registration_form.html',
+                  context={"form": form})
+
+
+def user_registration_request(request):
+    if request.method == 'POST':
+        return post_user_registration_request(request)
+    form = UserCreationForm()
+    return render(request,
+                  'forms/users/registration_form.html',
+                  context={"form": form})
 
 
 def logout_user(request):
