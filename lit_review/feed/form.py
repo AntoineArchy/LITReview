f1f361a7b5ef4094288076746a16c01a326c26e2
Archy <antoine.tassez@gmail.com>
2023-07-01 16:50:10 +0200

    Added views support for url redirection from feed and forms for model creation; all content is now included and all creation are now possible.

--- lit_review/feed/form.py
+++ lit_review/feed/form.py
@@ -1,9 +1,15 @@
 from django.forms import ModelForm
 
-from .models import Ticket
+from .models import Ticket, Review
 
 
 class TicketCreationForm(ModelForm):
     class Meta:
         model = Ticket
         fields = ['title', 'description', 'image']
+
+
+class ReviewCreationForm(ModelForm):
+    class Meta:
+        model = Review
+        fields = ['rating', 'headline', 'body']
--- lit_review/feed/views.py
+++ lit_review/feed/views.py
@@ -1,23 +1,28 @@
 from itertools import chain
 
 from django.db.models import Value, CharField
-from django.shortcuts import render
-from .form import TicketCreationForm
-from .models import Ticket
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.core.exceptions import ObjectDoesNotExist
+
+from .form import TicketCreationForm, ReviewCreationForm
+from .models import Ticket, Review
+
 
 # Create your views here.
 
 def get_users_viewable_reviews(user):
-    pass
+    return Review.objects.all()
+
 
 def get_users_viewable_tickets(user):
     return Ticket.objects.all()
 
 
-def user_feed(request):
-    # reviews = get_users_viewable_reviews(request.user)
+def render_user_feed(request):
+    reviews = get_users_viewable_reviews(request.user)
     # # returns queryset of reviews
-    # reviews = reviews.annotate(content_type=Value('REVIEW', CharField()))
+    reviews = reviews.annotate(content_type=Value('REVIEW', CharField()))
 
     tickets = get_users_viewable_tickets(request.user)
     # returns queryset of tickets
@@ -25,18 +30,104 @@ def user_feed(request):
 
     # combine and sort the two types of posts
     posts = sorted(
-        # chain(reviews, tickets),
-        chain(tickets, tickets),
+        chain(reviews, tickets),
         key=lambda post: post.time_created,
         reverse=True
     )
 
     return render(request,
                   "feed/home.html",
-                  context={"posts":posts})
+                  context={"posts": posts})
 
-def ticket_creation(request):
-    form = TicketCreationForm()
+
+def make_ticket(request):
+    form = TicketCreationForm(request.POST)
+    if form.is_valid():
+        new_ticket = form.save(commit=False)
+        new_ticket.user = request.user
+        return new_ticket
+    else:
+        for err_key, msg in form.errors.items():
+            messages.error(request, msg)
+    return None
+
+
+def make_review(request, ticket_id):
+    form = ReviewCreationForm(request.POST)
+    if form.is_valid():
+        new_review = form.save(commit=False)
+        new_review.user = request.user
+        new_review.ticket_id = ticket_id
+        return new_review
+    else:
+        for err_key, msg in form.errors.items():
+            messages.error(request, msg)
+    return None
+
+
+def post_create_new_ticket_request(request):
+    new_ticket = make_ticket(request)
+    if new_ticket is not None:
+        new_ticket.save()
+    return redirect("/")
+
+
+def create_new_ticket_request(request):
+    if request.method == "POST":
+        return post_create_new_ticket_request(request)
+    ticket_form = TicketCreationForm()
     return render(request,
-                  'feed/ticket_creation_page.html',
-                  context={'form': form})
+                  'feed/content_creation_page.html',
+                  context={'ticket_form': ticket_form,
+                           'user': request.user})
+
+
+def post_respond_to_ticket_request(request, ticket_id):
+    new_review = make_review(request, ticket_id)
+    if new_review is not None:
+        new_review.save()
+    return redirect("main:homepage")
+
+
+def respond_to_ticket_request(request, ticket_id):
+    try:
+        ticket = Ticket.objects.get(pk=ticket_id)
+    except ObjectDoesNotExist:
+        messages.error(request, "You tried to answer to a ticket who doesn't exist.")
+        return redirect('feed:review_creation')
+
+    if request.method == "POST":
+        return post_respond_to_ticket_request(request, ticket_id)
+    review_form = ReviewCreationForm()
+    return render(request,
+                  'feed/content_creation_page.html',
+                  context={'review_form': review_form,
+                           'ticket': ticket})
+
+
+def post_create_new_review_request(request):
+    new_ticket = make_ticket(request)
+    if new_ticket is not None:
+        new_review = make_review(request, new_ticket.pk)
+        if new_review is not None:
+            new_ticket.save()
+            new_review.ticket_id = new_ticket.pk
+            new_review.save()
+        return redirect('main:homepage')
+    review_form = ReviewCreationForm()
+    ticket_form = TicketCreationForm()
+    return render(request,
+                  'feed/content_creation_page.html',
+                  context={'review_form': review_form,
+                           'ticket_form': ticket_form})
+
+
+def create_new_review_request(request):
+    if request.method == "POST":
+        return post_create_new_review_request(request)
+    review_form = ReviewCreationForm()
+    ticket_form = TicketCreationForm()
+    return render(request,
+                  'feed/content_creation_page.html',
+                  context={'review_form': review_form,
+                           'ticket_form': ticket_form})
